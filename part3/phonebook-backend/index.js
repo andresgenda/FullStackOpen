42fb49a78c9cfa912daaa9c298b6c1e8d01028ae
andresgenda <andresgenda@gmail.com>
2025-07-22 23:23:59 -0600

    Finished exercises 3.1-3.6

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+        `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (!person) {
+    return response.status(404).end();
+  }
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1001);
+};
+
+const repeatedName = (name) => {
+  const lowerName = name.toLowerCase();
+  const person = persons.find((p) => p.name.toLowerCase() === lowerName);
+  return person;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error:
+        "Information missing - Please enter the name and the number of the person",
+    });
+  } else if (repeatedName(body.name)) {
+    return response.status(400).json({
+      error: "Name already in the phonebook - Name must be unique",
+    });
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
