73ff7fd3ff0db579757030985555032bce47d0fa
andresgenda <andresgenda@gmail.com>
2025-08-05 22:41:42 -0600

    Finished exercise 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,29 +8,6 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
   Contact.find({})
     .then((persons) => {
@@ -40,10 +17,14 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>
-        `);
+  Contact.find({})
+    .then((persons) => {
+      response.send(`
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()}</p>
+            `);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
