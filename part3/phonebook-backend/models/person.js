e10c6ffd02b669d1633e328915b1d1c0559c1853
andresgenda <andresgenda@gmail.com>
2025-08-19 20:17:01 -0600

    Finished exercise 3.20

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -21,7 +21,17 @@ const contactSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: `Phone does not follow the correct number form`,
+    },
+    required: true,
+  },
 });
 
 contactSchema.set("toJSON", {
